e1703e2d7eeb2c75900691b3f65791811217cedc
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-12-02 10:10:18 +0530

    changes

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // const Schema=mongoose.Schema()
 
 //  Your code goes here
 
-const mario=new mongoose.Schema({
-    name:{
-        type:String,
-        required:false
-    }, 
-    weight:{
-        type:Number,
-        required:false
-    }
-})
+const mario = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false
+  },
+  weight: {
+    type: Number,
+    required: false
+  }
+});
 
- const marioModel=mongoose.model('marioModel',mario);
+const marioModel = mongoose.model("marioModel", mario);
 
 module.exports = marioModel;
